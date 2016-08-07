a01146fe70a800e53a2889420c5af22b3213558d
mraafatphp <mmaestrorap@gmail.com>
2016-08-07 21:12:41 +0200

    Simple-MVC

---
+++ app/controllers/home.php
@@ -0,0 +1,23 @@
+<?php
+
+class Home Extends Controller
+{
+   protected $user;
+   public function __construct(){
+   	$this->user = $this->model('User');
+   	 }
+   public function index($name = ''){
+    $user = $this->user;
+    $user->name = $name;
+    $this->view('home/index',  ['name' => $user->name]);
+     }
+     public function create ($username ='', $email = '')
+     {
+     	$this->user->create([
+     		'username' => $username,
+     		'email' => $email
+     		]);
+
+     }
+	
+}
